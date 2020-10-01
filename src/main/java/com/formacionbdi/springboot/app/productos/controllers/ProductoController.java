2c93f47dd53948e45e0b157399521cd0218abe9d
Gwathlo <Gwathlo@gwathlo.cat>
2020-10-01 20:13:09 +0200

    Another way to get the port

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -3,7 +3,8 @@ package com.formacionbdi.springboot.app.productos.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.models.service.ProductoService;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 @RestController
 public class ProductoController {
 
-	public final Environment environment;
-	public final ProductoService productoService;
+	//public final Environment environment;
+	@Autowired
+	public ProductoService productoService;
+	
+	@Value("${server.port}")
+	private Integer port;
 	
 	@GetMapping("/listar")
 	List<Producto> listar() {
 		List<Producto> llista = productoService.findAll().stream().map(producto-> {
-			producto.setPort(environment.getProperty("local.server.port", Integer.class));
+			//producto.setPort(environment.getProperty("local.server.port", Integer.class));
+			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 		return llista;
@@ -33,7 +36,8 @@ public class ProductoController {
 	@GetMapping("/listar/{id}")
 	Producto obtenerProducto(@PathVariable Long id) {
 		Producto producto = productoService.findById(id);
-		producto.setPort(environment.getProperty("local.server.port", Integer.class));
+		//producto.setPort(environment.getProperty("local.server.port", Integer.class));
+		producto.setPort(port);
 		return producto;
 	}
 
